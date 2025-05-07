069a268b75320dbcadbc705ce0d1f7b2b58859f5
VivekB <136580365+GitVivekHub@users.noreply.github.com>
2025-05-07 12:34:03 +0530

    Revert "updated telemetry code"

--- prisma/migrations/20250506101923_device_metrics_update/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "DeviceMetrics" ALTER COLUMN "district" DROP NOT NULL,
-ALTER COLUMN "state" DROP NOT NULL;
--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,6 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { Logger } from 'nestjs-pino';
 import { Logger as NestLogger } from '@nestjs/common';
 import { AppClusterService } from './app-cluster.service';
-import { telemetryMiddleware } from "./telemetry/telemetryMiddleware";
 
 
 async function bootstrap() {
@@ -89,13 +88,6 @@ async function bootstrap() {
   await app.register(multipart);
   await app.register(compression, { encodings: ["gzip", "deflate"] });
   app.useStaticAssets({ root: join(__dirname, "../../fileUploads") });
-  // Register the telemetry middleware
-  // Add a hook to the specific route
-  app.getHttpAdapter().getInstance().addHook('preHandler', async (request, reply) => {
-    if (request.routerPath === '/prompt/:configid') {
-      await telemetryMiddleware(request, reply);
-    }
-  });
   await app.listen(3000, "0.0.0.0");
 }
 
